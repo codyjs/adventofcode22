aaaa5d78f71031dd635c037cc22a17fd7e8b3e7d
codyjs <codyjsimms@gmail.com>
2022-12-06 12:40:45 -0600

    Add day 6 solution

--- src/main/scala/day6/day6.scala
+++ src/main/scala/day6/day6.scala
@@ -2,6 +2,13 @@ package com.day6
 
 import scala.io.Source
 
-val inputFile = "src/main/resources/day5/input.txt"
+val inputFile = "src/main/resources/day6/input.txt"
+val markerSize = 14; // 4;
 
-def solution(): Unit = {}
+def solution(): Unit =
+  val count = Source.fromFile(inputFile)
+    .sliding(markerSize)
+    .map(str => str.toSet)
+    .takeWhile(set => set.size < markerSize)
+    .count(_ => true) + markerSize
+  println(count)
